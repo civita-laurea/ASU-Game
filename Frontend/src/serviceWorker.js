0f4f5af74590950d7c3fad68cb12afb7ffd76ab1
hsinjlee <hlee@294@asu.edu>
2021-02-28 15:19:16 -0700

    Update serviceWorker.js file

--- Frontend/src/serviceWorker.js
+++ Frontend/src/serviceWorker.js
@@ -130,6 +130,9 @@ const isLocalhost = Boolean(
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker.ready.then(registration => {
         registration.unregister();
+      })
+      .catch(error => {
+        console.error(error.message);
       });
     }
   }
