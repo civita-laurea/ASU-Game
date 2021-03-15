5b4e885f490c3d7c220185deb78cfb397880d8ea
hsinjlee <hlee294@asu.edu>
2021-03-15 07:37:27 -0700

    updated tile

--- Frontend/src/emojipedia.js
+++ Frontend/src/emojipedia.js
@@ -1,38 +1,39 @@
 const emojipedia = [
-    // {
-    //   id: 1,
-    //   emoji: "💪",
-    //   name: "Tense Biceps",
-    //   meaning:
-    //     "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
-    // },
+    
     {
       id: 1,
-      // emoji: "💪",
+      
       name: "Logic",
       meaning:
         "Stetch your analytic muscles with knights, knaves, logic gates, and more!"
     },
     {
       id: 2,
-      // emoji: "🙏",
+      
       name: "Computer Science Fundamentals",
       meaning:
         "Wrap your mind around computational thinking, from everyday tasks to algorithms."
     },
     {
       id: 3,
-      // emoji: "🤣",
+      
       name: "Introduction to Neural Networks",
       meaning:
         "Learn why neural networks are such flexible tools for learning."
     },
     {
       id: 4,
-      // emoji: "🤓",
+      
       name: "Mathematical Fundamentals",
       meaning:
         "The essential tools for mastering algebra, logic, and number theory!"
+    }
+    {
+      id: 5,
+      
+      name: "Scientific Thinking",
+      meaning:
+        "Open you eyes to the world around you by solving puzzles with science."
     }
   ];
   
