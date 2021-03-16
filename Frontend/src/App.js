ae388ecf58d7b24daf55ec6373bd765cc9280d60
mahendrarrao <mahendra.raok@gmail.com>
2021-03-16 13:35:12 +0530

    US115 Adding new dependencies

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react'
 import './App.css';
 import ButtonAppBar from './components/Header'
+import Sales from './components/Dashboard'
 import TitlebarGridList from './components/Cards'
 import MyButton from './components/Footer'
 import TitlebarGridListChallenges from './components/Challenges'
-
+import AddIcon from '@material-ui/icons/Add';
 import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 import IconButton from '@material-ui/core/IconButton';
 import { makeStyles } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -16,7 +18,7 @@ const useStyles = makeStyles((theme) => ({
   },
     // this group of buttons will be aligned to the right side
     toolbarButtons: {
-      marginLeft: 'auto',
+      marginRIght: 'auto',
     },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -37,9 +39,23 @@ class App extends Component {
     return (
       <div>
         <ButtonAppBar />
+        <Toolbar>
+        <h1>Dashboard</h1>
+        </Toolbar>
+        <Sales />
+        <Toolbar>
         <h1>My Courses</h1>
+        <IconButton aria-label="Add">
+            <AddIcon />
+        </IconButton>
+        </Toolbar>
         <TitlebarGridList />
+        <Toolbar>
         <h1>My Challenges</h1>
+        <IconButton aria-label="Add">
+            <AddIcon />
+        </IconButton>
+        </Toolbar>
         <TitlebarGridListChallenges />
         <MyButton/>
       </div>
