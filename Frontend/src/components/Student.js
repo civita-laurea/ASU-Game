26820d2b52e4860e564cd7065a80784e8d40033f
mahendrarrao <mahendra.raok@gmail.com>
2021-03-22 21:06:25 +0530

    US149 Dashboard

---
+++ Frontend/src/components/Student.js
@@ -0,0 +1,65 @@
+import React, {Component} from 'react'
+import '../App.css';
+import ButtonAppBar from './Header'
+import MapChart from './Dashboard'
+import TitlebarGridList from './Cards'
+import MyButton from './Footer'
+import TitlebarGridListChallenges from './Challenges'
+import AddIcon from '@material-ui/icons/Add';
+import IconButton from '@material-ui/core/IconButton';
+import { makeStyles } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+    // this group of buttons will be aligned to the right side
+    toolbarButtons: {
+      marginRIght: 'auto',
+    },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  h1: {
+    padding: 50,
+  },
+  h2:{
+    float: 'right',
+  },
+}));
+
+class Student extends Component {
+  render() {
+    return (
+      <div>
+        <ButtonAppBar />
+        <Toolbar>
+        <h1>Dashboard</h1>
+        </Toolbar>
+        <MapChart />
+        <Toolbar>
+        <h1>My Courses</h1>
+        <IconButton aria-label="Add">
+            <AddIcon />
+        </IconButton>
+        </Toolbar>
+        <TitlebarGridList />
+        <Toolbar>
+        <h1>My Challenges</h1>
+        <IconButton aria-label="Add">
+            <AddIcon />
+        </IconButton>
+        </Toolbar>
+        <TitlebarGridListChallenges />
+        <MyButton/>
+      </div>
+    );
+  }
+}
+
+export default Student;
