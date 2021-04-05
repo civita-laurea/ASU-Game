56329eecab0bde37c7f465aff62aced200741d78
mahendrarrao <mahendra.raok@gmail.com>
2021-04-05 10:25:09 -0700

    Reorganizing gameplay in location file

--- Frontend/src/components/DashData.js
+++ Frontend/src/components/DashData.js
@@ -5,7 +5,7 @@ const DashData = [
         fromY: 48.8566,
         toX: -74.006,
         toY: 40.7128,
-        stroke: "#00ff00",
+        stroke: "#FF5533",
     },
     {
         question: "Living in fear of any kind of near-human contact. Because when we could get rid of it by staying away, we didn't. That's why not doing the simple physical distancing practice, has made us practice social isolation like everyone else is a threat to our life.",
@@ -18,17 +18,17 @@ const DashData = [
 
     {
         question: "This is the situation in the beginning. People didn't take it seriously. Governments were afraid of the economical collapse. They little knew that they may have lost the force behind the economy, PEOPLE!, When it became pandemic and went viral, humanity knew that science can be the savior.All of the scientists started to act on it. And they knew that understanding the data is the way to go.",
-        fromX: 2.3522,
-        fromY: 48.8566,
-        toX: -58.3816,
-        toY: -34.6037,
+        fromX: -58.3816,
+        fromY: -34.6037,
+        toX: 28.04363,
+        toY: -26.20227,
         stroke: "#FF5533",
         
     },
     {
         question: "Find the solution and explain it through a universal language so everyone understands the world: THE MATHEMATICS.",
-        fromX: -74.006,
-        fromY: 40.7128,
+        fromX: 28.04363,
+        fromY: -26.20227,
         toX: 2.3522,
         toY: 48.8566,
         stroke: "#FF5533",
