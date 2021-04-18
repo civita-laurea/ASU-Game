bd8a3239ab0313c3a928863b1ba4fd4c37e9de64
hsinjlee <hlee294@asu.edu>
2021-04-18 01:37:32 -0700

    Create app.js

---
+++ csv/public/javascripts/app.js
@@ -0,0 +1,25 @@
+$(function(){
+
+    $("#fetchdata").on('click', function(){
+        $.get( "/fetchdata", function( data ) {
+            var products = data['data'];
+            $("#trdata").html('');
+            $("#message").hide();
+            var string = '';
+            $.each(products, function(index, product ) {
+
+                // string += '<tr><td>'+(index+1)+'</td><td>'+product['_id']+'</td><td>'+product['name']+'</td><td>'+product['category']+'</td><td>'+product['price']+'</td><td>'+product['manufacturer']+'</td></tr>';
+                string += '<tr><td>'+(index+1)+'</td><td>'+product['question']+'</td><td>'+product['option1']+'</td><td>'+product['option2']+'</td><td>'+product['option3']+'</td><td>'+product['option4']+'</td></tr>';
+            });
+
+            $("#trdata").html(string);
+        });
+    });
+ 
+    $("#importdata").on('click', function(){
+        $.get( "/import", function( data ) {
+            $("#message").show().html(data['success']);
+        });
+    });
+
+}); 
