e0b2e36374c5a9f95e9e6cb1900c64477eb482c7
hsinjlee <hlee294@asu.edu>
2021-04-18 07:46:09 -0700

    clean code

--- csv/public/javascripts/app.js
+++ csv/public/javascripts/app.js
@@ -8,7 +8,7 @@ $(function(){
             var string = '';
             $.each(products, function(index, product ) {
 
-                // string += '<tr><td>'+(index+1)+'</td><td>'+product['_id']+'</td><td>'+product['name']+'</td><td>'+product['category']+'</td><td>'+product['price']+'</td><td>'+product['manufacturer']+'</td></tr>';
+                
                 string += '<tr><td>'+(index+1)+'</td><td>'+product['question']+'</td><td>'+product['option1']+'</td><td>'+product['option2']+'</td><td>'+product['option3']+'</td><td>'+product['option4']+'</td></tr>';
             });
 
